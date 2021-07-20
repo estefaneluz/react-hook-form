959a5720d26b5480e5e03a456f1053f5840539e2
estefaneluz <estefanecortes@gmail.com>
2021-07-20 20:31:12 -0300

    formulario criado

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      
+      <form>
+        <label htmlFor="">Title</label>
+        <input type="text"/>      
+
+        <label htmlFor="">Body</label>
+        <input type="text"/>  
+
+        <label htmlFor="">Id do Usuário:</label>
+        <input type="number"/>
+
+        <button>Enviar</button>
+      </form>
     </div>
   );
 }
