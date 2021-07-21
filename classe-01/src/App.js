5eadd53ed18c25427202deaa6f5a6aa8f44d0c20
estefaneluz <estefanecortes@gmail.com>
2021-07-20 21:04:59 -0300

    enviar dados para API JSONPlaceholder

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -5,7 +5,19 @@ function App() {
   const { register, handleSubmit } = useForm();
 
   function onSubmit(data){
-    console.log(data)
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: data.title,
+        body: data.body,
+        userId: data.userId,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
   }
 
   return (
