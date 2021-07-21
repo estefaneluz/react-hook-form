618bd9dc6603da96ce65c143ce7d897a5353e294
estefaneluz <estefanecortes@gmail.com>
2021-07-21 16:35:29 -0300

    add Link do react-router-dom no menu

--- classe-01/src/components/MenuLateral/index.js
+++ classe-01/src/components/MenuLateral/index.js
@@ -1,25 +1,15 @@
-import "./style.css";
-import { Link } from "react-router-dom";
+import './style.css';
+import {Link} from 'react-router-dom'
 
 export default function MenuLateral() {
   return (
     <aside className="menu-lateral">
       <ul>
-        <Link to="/">
-          <li>Inicio</li>
-        </Link>
-        <Link to="/pesquisar">
-          <li>Pesquisar</li>
-        </Link>
-        <Link to="/cadastro">
-          <li>Cadastrar</li>
-        </Link>
-        <Link to="/editar">
-          <li>Editar</li>
-        </Link>
-        <Link to="/deletar">
-          <li>Deletar</li>
-        </Link>
+				<Link to="/"><li>Inicio</li></Link>
+        <Link to="/pesquisar"><li>Pesquisar</li></Link>
+        <Link to="/cadastro"><li>Cadastrar</li></Link>
+        <Link to="/editar"><li>Editar</li></Link>
+        <Link to="/deletar"><li>Deletar</li></Link>
       </ul>
     </aside>
   );
