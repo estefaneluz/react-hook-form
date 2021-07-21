9b3ca114863b08930f65ec2c9a9899901e0db5c7
estefaneluz <estefanecortes@gmail.com>
2021-07-21 20:21:30 -0300

    add mensagem de sucesso em deletar

--- classe-01/src/pages/deletar/index.js
+++ classe-01/src/pages/deletar/index.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import Input from '../../components/Input'
 
 const Page = () => {
     const {register, formState: {errors}, handleSubmit} = useForm();
+    const [success, setSuccess] = useState('');
 
     function onSubmit({id}){
+        setSuccess('');
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
           method: "DELETE",
-        }).then(() => console.log('Postagem deletada.'));
+        }).then(() => setSuccess('Postagem deletada.'));
     }
+
+    useEffect(() =>{
+      setSuccess('');
+    }, [errors.id]);
 
     return(
       <div className="container">
-          <h1>Delete uma Postagem</h1>
+        <h1>Delete uma Postagem</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Input 
             label="Informe o ID:"
@@ -21,9 +27,11 @@ const Page = () => {
             type="number"  
             {...register('id', { required: true, valueAsNumber: true })}
           />
-          {errors.id?.type === 'required' && <span className="error">É preciso informar o ID.</span>}
+          {errors.id?.type === 'required' && <span className="error" >É preciso informar o ID.</span>}
           <button>Deletar</button>
         </form>
+
+        <p className="success">{success}</p>
       </div>
     );
 }
