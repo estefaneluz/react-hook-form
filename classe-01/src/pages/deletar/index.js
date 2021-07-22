ad9c564515b5178b4ddb62a07a2a740e94d5b8a5
estefaneluz <estefanecortes@gmail.com>
2021-07-22 16:33:06 -0300

    ajuste identacao da pag deletar

--- classe-01/src/pages/deletar/index.js
+++ classe-01/src/pages/deletar/index.js
@@ -1,38 +1,44 @@
-import { useState, useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import Input from '../../components/Input'
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import Input from "../../components/Input";
 
 const Page = () => {
-    const {register, formState: {errors}, handleSubmit} = useForm();
-    const [success, setSuccess] = useState('');
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const [success, setSuccess] = useState("");
 
-    function onSubmit({id}){
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-          method: "DELETE",
-        }).then(() => setSuccess('Postagem deletada.'));
-    }
+  function onSubmit({ id }) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE",
+    }).then(() => setSuccess("Postagem deletada."));
+  }
 
-    useEffect(() =>{
-      setSuccess('');
-    }, [errors.id]);
+  useEffect(() => {
+    setSuccess("");
+  }, [errors.id]);
 
-    return(
-      <div className="container">
-        <h1>Delete uma Postagem</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input 
-            label="Informe o ID:"
-            id="id" 
-            type="number"  
-            {...register('id', { required: true, valueAsNumber: true })}
-          />
-          {errors.id?.type === 'required' && <span className="error" >É preciso informar o ID.</span>}
-          <button>Deletar</button>
-        </form>
+  return (
+    <div className="container">
+      <h1>Delete uma Postagem</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          label="Informe o ID:"
+          id="id"
+          type="number"
+          {...register("id", { required: true, valueAsNumber: true })}
+        />
+        {errors.id?.type === "required" && (
+          <span className="error">É preciso informar o ID.</span>
+        )}
+        <button>Deletar</button>
+      </form>
 
-        <p className="success">{success}</p>
-      </div>
-    );
-}
+      <p className="success">{success}</p>
+    </div>
+  );
+};
 
 export default Page;
