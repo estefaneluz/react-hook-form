3024e263e706eeb8d5aa27d4b90400cdcb32cca5
estefaneluz <estefanecortes@gmail.com>
2021-07-22 16:35:22 -0300

    ajuste identacao routes.js

--- classe-01/src/Routes.js
+++ classe-01/src/Routes.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Cadastro from './pages/cadastro'
-import Pesquisar from './pages/pesquisar'
-import Deletar from './pages/deletar'
-import Editar from './pages/editar'
-import Inicio from './pages/inicio'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Cadastro from "./pages/cadastro";
+import Pesquisar from "./pages/pesquisar";
+import Deletar from "./pages/deletar";
+import Editar from "./pages/editar";
+import Inicio from "./pages/inicio";
 
-export const urlBase = '/react-hook-form'
+export const urlBase = "/react-hook-form";
 
 export default function Routes() {
-    return (
-        <Switch>
-            <Route path={urlBase} exact component={Inicio}/>
-
-            <Route path={`${urlBase}/cadastro`} component={Cadastro}/>
-
-            <Route path={`${urlBase}/pesquisar`} component={Pesquisar}/>
-
-            <Route path={`${urlBase}/editar`} component={Editar}/>
-
-            <Route path={`${urlBase}/deletar`} component={Deletar}/>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path={urlBase} exact component={Inicio} />
+      <Route path={`${urlBase}/cadastro`} component={Cadastro} />
+      <Route path={`${urlBase}/pesquisar`} component={Pesquisar} />
+      <Route path={`${urlBase}/editar`} component={Editar} />
+      <Route path={`${urlBase}/deletar`} component={Deletar} />
+    </Switch>
+  );
 }
